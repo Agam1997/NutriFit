ea9e86a73891b7e92d79a0d28fdb3da0d7d4b08f
Agam1997 <hello.world.agam@gmail.com>
2024-02-04 15:02:02 +0530

    fix(): fixed food.route.js file added controllers to route

--- backend/src/routes/v1/food.route.js
+++ backend/src/routes/v1/food.route.js
@@ -2,18 +2,17 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const foodValidation = require('../../validations/food.validation');
-const foodController = require("../../controllers/food.controller");
+const foodController = require('../../controllers/food.controller');
 
 const router = express.Router();
 
 router
-    .route('/')
-    .get(auth('getFoods'), validate(foodValidation.getFoods), foodController.getAllFoods)
-    .post(auth('manageFoods'), validate(foodValidation.createFood), foodController.createFood)
+  .route('/')
+  .get(auth('getFoods'), validate(foodValidation.getFoods), foodController.getAllFoods)
+  .post(auth('manageFoods'), validate(foodValidation.createFood), foodController.createFood);
 
 router
-    .route('/:foodId')
-    .get(auth('getFoods'), validate(foodValidation.getFoods), foodController.getFoodById)
-    .put(auth('manageFoods'))
-    .delete(auth('manageFoods'))
-
+  .route('/:foodId')
+  .get(auth('getFoods'), validate(foodValidation.getFood), foodController.getFoodById)
+  .put(auth('manageFoods'), validate(foodValidation.createFood), foodController.updateFoodById)
+  .delete(auth('manageFoods'), validate(foodValidation.deleteFood), foodController.deleteFood);
